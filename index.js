fb97f24a3d8e982b6d6be2d710c58436c7ba3684
MichaelCapo23 <123pie23@gmail.com>
2019-01-02 13:28:32 -0800

    made a proxy, install nodemon and npm-run-all

--- index.js
+++ index.js
@@ -4,13 +4,44 @@ const PORT = process.env.PORT || 9000;
 
 const app = express();
 app.use(cors()); // set the request header to * so itll talk to any server that tries to communicate with it (your other server)
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 app.get('/', (request, response) => {
     response.send('<h1>The server is working!</h1>');
 });
+
+app.get('/api/test', (request, response) => {
+   const data = {
+       success: true,
+       message: "Test API working",
+       another: "property goes here"
+   };
+   response.send(data);
+});
+
+app.get('/api/user', (request, response) => {
+   const user = {
+       email: 'jimbean.com',
+       name: "Jim Bean",
+       username: "jimIsABean"
+   };
+   response.send(user);
+});
+
+app.post("/api/send-message", (request, response) => {
+   console.log("data from client: ", request.body);
+   response.send({
+       success: true,
+       data: request.body,
+   });
+});
+
 
 app.listen(PORT, () => {
    console.log("server running on localhost port: " +  PORT);
 }).on("error", (err) => {
     console.log("server listen error, do you already have a server running on port: " + PORT);
 });
+
